a929825c5e99480dcd8ae1ce3cb8a6367bd3f2f2
Nuttanont Na Ranong <y07025@autox.co.th>
2024-04-27 18:37:23 +0700

    encap bcrypt func

---
+++ src/utils/bcrypt.ts
@@ -0,0 +1,13 @@
+import { compareSync, genSaltSync, hashSync } from "bcrypt";
+
+export const hashPassword = (password: string) => {
+	const salt = genSaltSync(12);
+	return hashSync(password, salt);
+};
+
+export const verifyPassword = (
+	plainPassword: string,
+	hashedPassword: string
+) => {
+	return compareSync(plainPassword, hashedPassword);
+};
