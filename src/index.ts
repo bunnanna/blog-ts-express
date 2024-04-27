9ab67791ee6cf0d434aaf0fa45b0a42b02cd23eb
Nuttanont Na Ranong <y07025@autox.co.th>
2024-04-27 20:45:30 +0700

    add user router to app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { configDotenv } from "dotenv";
 import express from "express";
+import userRouter from "./app/user/controller/UserController";
 configDotenv();
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use("/users", userRouter);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
