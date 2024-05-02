5662861b8240e0e18bf6a27f7704bd25f2e86514
Nuttanont Na Ranong <y07025@autox.co.th>
2024-05-02 22:22:37 +0700

    merge user router controller

--- src/app/user/controllers/UserController.ts
+++ src/app/user/controllers/UserController.ts
@@ -1,17 +1,25 @@
+import { Router } from "express";
 import { validationResult } from "express-validator";
 import { inject, injectable } from "inversify";
 import { userIdentifier } from "../di/userIdentifiers";
+import { registerValidator } from "../middlewares/validator";
 import { IRegisterUseCase } from "../usecase/RegisterUseCase/IRegisterUsecase";
 import { IUserController } from "./IUserController";
 
 @injectable()
 export default class UserController implements IUserController {
+	public router: Router;
 	constructor(
 		@inject(userIdentifier.IRegisterUseCase)
 		private registerUseCase: IRegisterUseCase
 	) {
+		this.router = Router();
+		this.apply();
 		console.log(`User Controller Created`);
 	}
+	apply: () => void = () => {
+		this.router.post("/", registerValidator, this.register);
+	};
 
 	register: IUserController["register"] = async (req, res) => {
 		const validationErrors = validationResult(req);
