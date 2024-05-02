60c82c2fab430cd6a5d2c8dfaf0803155b62e6f9
Nuttanont Na Ranong <y07025@autox.co.th>
2024-05-02 14:45:57 +0700

    add db identifier

--- src/core/dbConnection/prisma.ts
+++ src/core/dbConnection/prisma.ts
@@ -2,3 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export default prisma;
+export const dbIdentifier = {
+	PrismaClient: Symbol.for("PrismaClient"),
+};
