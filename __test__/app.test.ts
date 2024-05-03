61db36ff2df47389656b75eec2623b93b10e6eda
Nuttanont Na Ranong <y07025@autox.co.th>
2024-05-03 21:53:26 +0700

    prettier fix

--- __test__/app.test.ts
+++ __test__/app.test.ts
@@ -1,7 +1,8 @@
-import app from "@src/app";
-import request from "supertest";
-describe("app", () => {
-	test("health", async () => {
-		await request(app).get("/").expect(200).expect("Hello World!");
-	});
+import app from '@src/app';
+import request from 'supertest';
+
+describe('app', () => {
+  test('health', async () => {
+    await request(app).get('/').expect(200).expect('Hello World!');
+  });
 });
