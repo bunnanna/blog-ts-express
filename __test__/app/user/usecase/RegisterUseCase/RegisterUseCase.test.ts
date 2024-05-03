08f817a0e8373b7bfd531a32713a93c8d6482dd3
Nuttanont Na Ranong <y07025@autox.co.th>
2024-05-03 22:58:06 +0700

    add register usecase test

---
+++ __test__/app/user/usecase/RegisterUseCase/RegisterUseCase.test.ts
@@ -0,0 +1,49 @@
+import 'reflect-metadata';
+
+import IUserRepository from '@src/app/user/repositories/IUserRepository';
+import IRegisterUseCase from '@src/app/user/usecase/RegisterUseCase/IRegisterUsecase';
+import RegisterUseCase from '@src/app/user/usecase/RegisterUseCase/RegisterUseCase';
+
+class MockUserRepository implements IUserRepository {
+	getAll: IUserRepository['getAll'] = () => {
+		throw new Error('Not Implemented Method');
+	};
+
+	getById: IUserRepository['getById'] = () => {
+		throw new Error('Not Implemented Method');
+	};
+
+	create: IUserRepository['create'] = () => {
+		throw new Error('Not Implemented Method');
+	};
+
+	update: IUserRepository['update'] = () => {
+		throw new Error('Not Implemented Method');
+	};
+}
+
+describe('RegisterUseCase', () => {
+	let mockUserRepository: MockUserRepository;
+	let registerUseCase: IRegisterUseCase;
+
+	beforeAll(() => {
+		mockUserRepository = new MockUserRepository();
+		registerUseCase = new RegisterUseCase(mockUserRepository);
+	});
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+	describe('register', () => {
+		test('should register completely', async () => {
+			const InputData = {
+				username: 'string',
+				email: 'string@string.com',
+				password: 'String123',
+			};
+			jest.spyOn(mockUserRepository, 'create').mockImplementation(() => Promise.resolve());
+			const OutputData = await registerUseCase.execute(InputData);
+			expect(OutputData).toBeUndefined();
+			expect(mockUserRepository.create.call).toHaveLength(1);
+		});
+	});
+});
