8bf3d8f6ea2114aa2f42a4ca255850211a9c414a
Nuttanont Na Ranong <y07025@autox.co.th>
2024-05-07 00:20:02 +0700

    change type at usecase

--- __test__/app/user/usecase/GetUserUseCase/GetUserUseCase.test.ts
+++ __test__/app/user/usecase/GetUserUseCase/GetUserUseCase.test.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 
-import { User } from '@prisma/client';
+import { UserEntity } from '@src/app/user/entities/user';
 import IUserRepository from '@src/app/user/repositories/IUserRepository';
 import GetUserUseCase from '@src/app/user/usecase/GetUserUseCase/GetUserUseCase';
 import { mockDeep, mockReset } from 'jest-mock-extended';
@@ -21,16 +21,15 @@ describe('GetUserUseCase', () => {
 	describe('execute', () => {
 		test('should return value', async () => {
 			const userId = '16478cbe-9741-4c96-9a0c-177c7635da74';
-			const userData: User = {
+			const userData: UserEntity = {
 				username: 'string',
 				email: 'string@string.com',
-				password: '',
 				updatedAt: new Date('2024-05-02T03:10:41.027Z'),
 				createdAt: new Date('2024-05-02T03:10:41.027Z'),
 				userId: '16478cbe-9741-4c96-9a0c-177c7635da74',
-				role: 'User',
+				role: 'User'
 			};
-			jest.spyOn(mockUserRepository, 'getById').mockImplementation(async () => userData);
+			jest.spyOn(mockUserRepository, 'getById').mockResolvedValue(userData);
 			const result = await getUserUseCase.execute(userId);
 			expect(result).not.toBeDefined();
 			expect(result).not.toHaveProperty('password');
