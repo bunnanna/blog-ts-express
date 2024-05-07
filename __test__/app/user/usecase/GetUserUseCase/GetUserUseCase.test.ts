dd66c539b76e41509d1f230ec24f7f8fff040b52
Nuttanont Na Ranong <y07025@autox.co.th>
2024-05-07 09:52:52 +0700

    impl get user usecase

--- __test__/app/user/usecase/GetUserUseCase/GetUserUseCase.test.ts
+++ __test__/app/user/usecase/GetUserUseCase/GetUserUseCase.test.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 
-import { UserEntity } from '@src/app/user/entities/user';
+import { UserEntityWithOutPassword } from '@src/app/user/entities/user';
 import IUserRepository from '@src/app/user/repositories/IUserRepository';
 import GetUserUseCase from '@src/app/user/usecase/GetUserUseCase/GetUserUseCase';
 import { mockDeep, mockReset } from 'jest-mock-extended';
@@ -21,7 +21,7 @@ describe('GetUserUseCase', () => {
 	describe('execute', () => {
 		test('should return value', async () => {
 			const userId = '16478cbe-9741-4c96-9a0c-177c7635da74';
-			const userData: UserEntity = {
+			const userData: UserEntityWithOutPassword = {
 				username: 'string',
 				email: 'string@string.com',
 				updatedAt: new Date('2024-05-02T03:10:41.027Z'),
@@ -31,7 +31,7 @@ describe('GetUserUseCase', () => {
 			};
 			jest.spyOn(mockUserRepository, 'getById').mockResolvedValue(userData);
 			const result = await getUserUseCase.execute(userId);
-			expect(result).not.toBeDefined();
+			expect(result).toBeDefined();
 			expect(result).not.toHaveProperty('password');
 		});
 	});
