e56e55078bbd88a81abecbeac2290c71dfce8007
Nuttanont Na Ranong <y07025@autox.co.th>
2024-05-07 17:35:18 +0700

    change jwt algorithm

--- src/core/class/JWTService/JWTAccessService.ts
+++ src/core/class/JWTService/JWTAccessService.ts
@@ -8,7 +8,7 @@ export default class JWTAccessService implements IJWTService {
 		if (!secret) throw new Error('jwt secret is undefined.');
 	}
 
-	sign: IJWTService['sign'] = (obj) => sign(obj, this.secret, { algorithm: 'RS512', expiresIn: '1m' });
+	sign: IJWTService['sign'] = (obj) => sign(obj, this.secret, { algorithm: 'HS512', expiresIn: '1m' });
 
 	verify: IJWTService['verify'] = (token) => verify(token, this.secret) as JwtPayload;
 }
--- src/core/class/JWTService/JWTRefreshService.ts
+++ src/core/class/JWTService/JWTRefreshService.ts
@@ -8,7 +8,7 @@ export default class JWTRefreshService implements IJWTService {
 		if (!secret) throw new Error('jwt secret is undefined.');
 	}
 
-	sign: IJWTService['sign'] = (obj) => sign(obj, this.secret, { algorithm: 'RS512', expiresIn: '1w' });
+	sign: IJWTService['sign'] = (obj) => sign(obj, this.secret, { algorithm: 'HS512', expiresIn: '1w' });
 
 	verify: IJWTService['verify'] = (token) => verify(token, this.secret) as JwtPayload;
 }
