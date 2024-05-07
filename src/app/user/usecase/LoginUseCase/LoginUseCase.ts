d7c7982fc50de05001252dcd67e858726b5fccfe
Nuttanont Na Ranong <y07025@autox.co.th>
2024-05-07 17:36:12 +0700

    impl login usecase

--- src/app/user/usecase/LoginUseCase/LoginUseCase.ts
+++ src/app/user/usecase/LoginUseCase/LoginUseCase.ts
@@ -1,3 +1,7 @@
+import 'reflect-metadata';
+
+import { EncryptService } from '@src/core/class/EncryptService/EncryptService';
+import { BadRequestError } from '@src/core/class/Error';
 import { IJWTService } from '@src/core/class/JWTService/IJWTService';
 import { coreIdentifier } from '@src/core/di/coreIdentifier';
 import { inject, injectable } from 'inversify';
@@ -9,10 +13,16 @@ import { ILoginUseCase } from './ILoginUsecase';
 export default class LoginUseCase implements ILoginUseCase {
 	constructor(
 		@inject(userIdentifier.IUserRepository) private repository: IUserRepository,
-		@inject(coreIdentifier.JWTRefreshService) private jwt: IJWTService
+		@inject(coreIdentifier.JWTRefreshService) private jwt: IJWTService,
+		@inject(coreIdentifier.EncryptService) private encryptService: EncryptService
 	) {}
 
-	execute: ILoginUseCase['execute'] = async () => {
-		throw new Error('Not Implemented Method');
+	execute: ILoginUseCase['execute'] = async (loginBody) => {
+		const { email, password } = loginBody;
+		const userData = await this.repository.getByEmail(email);
+		if (!this.encryptService.verify(password, userData.password))
+			throw new BadRequestError('invalid username or password');
+
+		return this.jwt.sign({ userId: userData.userId });
 	};
 }
